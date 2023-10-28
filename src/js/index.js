cab2965c61c9eb8563666794edf226112dbf8f91
Himanshu Singh <himanshu.developer01@gmail.com>
2023-10-28 09:19:48 +0530

    completed entire game

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,58 @@ const leftBtn = document.querySelector('#left');
 const homeBtn = document.querySelector('#home');
 const turnClockwiseBtn = document.querySelector('#turnClockWise');
 const turnAntiClockwiseBtn = document.querySelector('#turnAntiClockWise');
+
+
+let rotation = 0;
+let xPos = 0;
+let yPos = 0;
+
+const turnClockWise = () => {
+    rotation += 90;
+    bug.style.transform = `rotate(${rotation}deg)`;
+};
+  
+const turnAntiClockWise = () => {
+    rotation += -90;
+    bug.style.transform = `rotate(${rotation}deg)`;
+};
+
+const setTransform = () => {
+    bug.style.left = `${xPos}px`;
+    bug.style.top = `${yPos}px`;
+}
+
+const stepLeft = () => {
+    xPos > 0 && (xPos += -10);
+    setTransform();
+}
+
+const stepRight = () => {
+    xPos < 450 && (xPos+=10);
+    setTransform();
+}
+
+const stepUp =()=>{
+    yPos > 0 && (yPos+=-10);
+    setTransform();
+}
+
+const stepDown = () => {
+    yPos < 450 && (yPos += 10);
+    setTransform();
+}
+
+const home = () => {
+    xPos= 0;
+    yPos=0;
+    setTransform();
+    bug.style.transform = `rotate(${0}deg)`
+}
+
+upBtn.addEventListener('click', stepUp);
+downBtn.addEventListener('click', stepDown);
+rightBtn.addEventListener('click', stepRight);
+leftBtn.addEventListener('click',stepLeft);
+turnClockwiseBtn.addEventListener('click',turnClockWise);
+turnAntiClockwiseBtn.addEventListener('click', turnAntiClockWise);
+homeBtn.addEventListener('click',home);
